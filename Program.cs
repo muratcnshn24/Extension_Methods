05326e4747f8bd98e5c8c60841231adecc1b2d59
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 00:43:58 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Extension_Methods
+{
+
+    public static class SrtingExtension
+    {
+        public static int KelimeSayisi(this string s) =>
+            s.Split().Length;
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Merhaba Güzel Dünya";   //keilme yazdikca veya cıkardikca kelime sayisini veriyor.
+            Console.WriteLine($"{s.KelimeSayisi()}");
+        }
+    }
+}
